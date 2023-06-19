4e4fb26b3397979227f6d045200bbaf436d916d0
alissonkhr <alissonricardo68@gmail.com>
2023-06-18 20:12:20 -0700

    title font is bold

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
               d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
             />
           </svg>
-          <span>iTravel</span>
+          <span className="font-bold">iTravel</span>
         </a>
       </header>
     </div>
