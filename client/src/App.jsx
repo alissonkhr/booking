19927c3f2da5ce3c8a0cf0e6dedebc28767286cf
alissonkhr <alissonricardo68@gmail.com>
2023-06-23 20:12:20 -0700

    t styling done to logo and searchbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <header className="p-4">
+      <header className="p-4 flex justify-between">
         <a href="" className="flex items-center gap-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -21,15 +21,25 @@ function App() {
           </svg>
           <span className="font-bold text-xl">iTravel</span>
         </a>
-        <div>
+        <div className="flex">
           <div>Anywhere</div>
           <div>Any week</div>
           <div>Add guests</div>
           <button>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-</svg>
-
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
           </button>
         </div>
       </header>
