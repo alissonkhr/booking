c494dbe80dbb5e8f3fa5be9e8030b265b078aecb
alissonkhr <alissonricardo68@gmail.com>
2023-07-08 20:12:20 -0700

    styling to register page link on login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -10,8 +10,11 @@ function LoginPage() {
           <input type="email" placeholder="your@email.com" />
           <input type="password" placeholder="password" />
           <button className="primary">Login</button>
-          <div>
-            Don't have an account yet? <Link to={"/register"}>Register</Link>
+          <div className="text-center py-2 text-gray-500">
+            Don't have an account yet?{" "}
+            <Link className="underline text-black" to={"/register"}>
+              Register now
+            </Link>
           </div>
         </form>
       </div>
