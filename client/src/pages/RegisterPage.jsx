f4062d642a9e51c8f19ac76312edd22105becc44
alissonkhr <alissonricardo68@gmail.com>
2023-07-11 20:12:20 -0700

    added more to the register form

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,6 +7,7 @@ function RegisterPage() {
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-md mx-auto">
+        <input type="text" placeholder="John Doe" />
           <input type="email" placeholder="your@email.com" />
           <input type="password" placeholder="password" />
           <button className="primary">Register</button>
