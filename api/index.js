113b5520b814c5cd3f4028f32cb89976a17788bb
alissonkhr <alissonricardo68@gmail.com>
2023-07-21 20:12:20 -0700

    added dotenv and mongo url to api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
@@ -11,6 +14,8 @@ app.use(
   })
 );
 
+console.log(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 // mongodb db pw: Do1nJCgC4TuMKTGr
 
 app.get("/test", (req, res) => {
