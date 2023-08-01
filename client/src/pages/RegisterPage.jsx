181eac7b66a06b6cc4416e586d8411e41e23eb84
alissonkhr <alissonricardo68@gmail.com>
2023-07-31 20:12:20 -0700

    caught errors for registration form on submit

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,13 +7,18 @@ function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function registerUser(ev) {
+  async function registerUser(ev) {
     ev.preventDefault();
-    axios.post("/register", {
-      name,
-      email,
-      password,
-    });
+    try {
+      await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+      alert("Registration successful. You may now log in!");
+    } catch (e) {
+      alert("Registration failed. Please try again later.");
+    }
   }
   return (
     <div className="mt-4 grow flex items-center justify-around">
