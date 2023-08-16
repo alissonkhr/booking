aab50b4368d1d402bca8ffc5fb3f249a2d628f45
alissonkhr <alissonricardo68@gmail.com>
2023-08-15 20:12:20 -0700

    changed context component for users to use provider

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,7 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
-  return { children };
+  const [user, setUser] = useState(null);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
