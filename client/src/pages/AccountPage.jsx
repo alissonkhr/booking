389ef1db9db081629e0ec917a506d4906fc2b945
alissonkhr <alissonricardo68@gmail.com>
2023-09-01 11:03:41 -0700

    added links to account page for users to see and some styling

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { UserContext } from "../UserContext.jsx";
 
 export default function AccountPage() {
@@ -12,5 +12,13 @@ export default function AccountPage() {
   if (ready && !user) {
     return <Navigate to={"/login"} />;
   }
-  return <div>Account Page for {user?.name}</div>;
+  return (
+    <div>
+      <nav className="w-full flex justify-center mt-8 gap-2">
+        <Link className="py-2 px-6 bg-primary text-white rounded-full" to={"/account"}>My Profile</Link>
+        <Link className="py-2 px-6" to={"/account/bookings"}>My Bookings</Link>
+        <Link className="py-2 px-6" to={"/account/places"}>My Accomodations</Link>
+      </nav>
+    </div>
+  );
 }
