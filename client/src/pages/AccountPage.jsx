2e0cd1bce8296e84eb657aff5c60f1ed95455491
alissonkhr <alissonricardo68@gmail.com>
2023-08-31 17:45:17 -0700

    user account page now has conditionals set up for if user is logged in and info is ready

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,7 +1,16 @@
 import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 import { UserContext } from "../UserContext.jsx";
 
 export default function AccountPage() {
-  const { user } = useContext(UserContext);
-  return <div>Account Page for {user.name}</div>;
+  const { ready, user } = useContext(UserContext);
+
+  if (!ready) {
+    return "Loading...";
+  }
+
+  if (ready && !user) {
+    return <Navigate to={"/login"} />;
+  }
+  return <div>Account Page for {user?.name}</div>;
 }
