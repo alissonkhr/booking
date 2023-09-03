f790b426d04b20a6cb3c9c98adf86c0716ee3362
alissonkhr <alissonricardo68@gmail.com>
2023-09-02 18:21:09 -0700

    added working routes for account subpages and they console logged correctly

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,9 +1,12 @@
 import { useContext } from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 import { UserContext } from "../UserContext.jsx";
 
 export default function AccountPage() {
   const { ready, user } = useContext(UserContext);
+
+  const { subpage } = useParams();
+  console.log(subpage);
 
   if (!ready) {
     return "Loading...";
@@ -12,12 +15,22 @@ export default function AccountPage() {
   if (ready && !user) {
     return <Navigate to={"/login"} />;
   }
+
   return (
     <div>
       <nav className="w-full flex justify-center mt-8 gap-2">
-        <Link className="py-2 px-6 bg-primary text-white rounded-full" to={"/account"}>My Profile</Link>
-        <Link className="py-2 px-6" to={"/account/bookings"}>My Bookings</Link>
-        <Link className="py-2 px-6" to={"/account/places"}>My Accomodations</Link>
+        <Link
+          className="py-2 px-6 bg-primary text-white rounded-full"
+          to={"/account"}
+        >
+          My Profile
+        </Link>
+        <Link className="py-2 px-6" to={"/account/bookings"}>
+          My Bookings
+        </Link>
+        <Link className="py-2 px-6" to={"/account/places"}>
+          My Accomodations
+        </Link>
       </nav>
     </div>
   );
