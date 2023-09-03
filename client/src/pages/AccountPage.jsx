2e432f8f691313b3c435dd26e3049caea9c71845
alissonkhr <alissonricardo68@gmail.com>
2023-09-02 18:32:39 -0700

    refactored subpage variable conditional statement to make code simpler to understand

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -5,7 +5,10 @@ import { UserContext } from "../UserContext.jsx";
 export default function AccountPage() {
   const { ready, user } = useContext(UserContext);
 
-  const { subpage } = useParams();
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
 
   if (!ready) {
     return "Loading...";
@@ -17,7 +20,7 @@ export default function AccountPage() {
 
   function linkClasses(type = null) {
     let classes = "py-2 px-6";
-    if (type === subpage || (subpage === undefined && type === 'profile')) {
+    if (type === subpage) {
       classes += " bg-primary text-white rounded-full";
     }
     return classes;
