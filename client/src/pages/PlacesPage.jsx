72c41e1d76c83193139843a5d4e3fc88e216b18a
alissonkhr <alissonricardo68@gmail.com>
2023-09-15 20:12:20 -0700

    added keys to schema

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose");
 
-const PlaceSchema = new mongoose.Schema({});
+const PlaceSchema = new mongoose.Schema({
+  title: String,
+  address: String,
+  photos: [String],
+  description: String,
+  perks: [String],
+});
