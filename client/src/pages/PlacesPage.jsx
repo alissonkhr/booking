47d7828bed376fa920b03a55d4084e9c3d38d9ec
alissonkhr <alissonricardo68@gmail.com>
2023-09-18 20:12:20 -0700

    styling added to link to add a place

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -3,8 +3,13 @@ import { Link } from "react-router-dom";
 export default function PlacesPage() {
   return (
     <div>
-      <div className="">
-        <Link className="" to={"/account/places/new"}>Add new place</Link>
+      <div className="text-center">
+        <Link
+          className="bg-primary text-white py-2 px-6 rounded-full"
+          to={"/account/places/new"}
+        >
+          Add new place
+        </Link>
       </div>
       my places
     </div>
