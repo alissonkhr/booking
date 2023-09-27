e023dd1053a5aeba2cad234386c7f6a2fa8035d0
alissonkhr <alissonricardo68@gmail.com>
2023-09-26 20:12:20 -0700

    component conditionals to show form only when clicked to add new place

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,31 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function PlacesPage() {
+  const { action } = useParams();
   return (
     <div>
-      <div className="text-center">
-        <Link
-          className=" inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
-          to={"/account/places/new"}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
+      {action !== "new" && (
+        <div className="text-center">
+          <Link
+            className=" inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
+            to={"/account/places/new"}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
-          </svg>
-          Add new place
-        </Link>
-      </div>
-      my places
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+            Add new place
+          </Link>
+        </div>
+      )}
+      {action === "new" && (
+        <div>
+          <form>
+            <input type="text" placeholder="title" />
+          </form>
+        </div>
+      )}
     </div>
   );
 }
