ad89a64b8938e254ebfd263f304d4b9d433c8046
alissonkhr <alissonricardo68@gmail.com>
2023-11-26 20:12:20 -0800

    fixed bug with code not displaying perks properly

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,5 +1,5 @@
-import { Link, useParams } from "react-router-dom";
 import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import Perks from "../Perks";
 
 export default function PlacesPage() {
@@ -96,13 +96,13 @@ export default function PlacesPage() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="w-6 h-8"
+                  className="w-6 h-8"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
                   />
                 </svg>
@@ -116,7 +116,8 @@ export default function PlacesPage() {
             />
             {preInput("Perks", "Choose all the perks for your place!")}
             <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-              <Perks selected={perks} onChange={setPerks()} />
+              <Perks selected={perks} />
+              {/* add onChange={setPerks()} later to Perks component */}
             </div>
             {preInput("Extra Info", "House rules, etc.")}
             <textarea
